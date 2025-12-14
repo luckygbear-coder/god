0e57eed6b2918af5194486c7f36a135c34638ecf
luckygbear-coder <luckygbear@gmail.com>
2025-12-14 20:47:27 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE_NAME = "werewolf-god-v1";
-
-const FILES_TO_CACHE = [
+const CACHE = "werewolf-god-v1";
+const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
@@ -10,30 +9,18 @@ const FILES_TO_CACHE = [
   "./manifest.webmanifest"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
